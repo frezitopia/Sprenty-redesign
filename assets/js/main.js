00389a7da53ac7e541c06ee228dab94fc8d2b7e6
frezitopia <anri.axaladze@gmail.com>
2022-07-13 04:04:04 +0400

    explores part

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,40 @@
-$('.owl-carousel').owlCarousel(
-    {
+$('.sprenty-owlOne').owlCarousel({
     loop: true,
     margin: 25,
     dots: false,
-    responsive:{
-        320:{
+    responsive: {
+        320: {
             items: 1,
             center: true,
         },
-        430:{
+        430: {
             items: 2,
         },
-        768:{
+        768: {
             items: 3,
         },
-        1000:{
+        1000: {
             items: 4,
             mouseDrag: false,
             nav: false,
         },
     }
-}
-)
+})
 
+$('.sprenty-owlTwo').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:true,
+    dots: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4,
+        }
+    }
+})
